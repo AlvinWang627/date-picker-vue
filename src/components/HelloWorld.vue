92b9df825865b5cd8d07e30fdce5c74488ed70c7
Alvin <AlvinWang627@gmail.com>
2022-11-15 00:29:32 +0800

    date-picker practice

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <date-picker
+      v-model="value1"
+      format="YYYY-MM-DD dddd"
+      type="date"
+      placeholder="開始時間"
+      :disabled-date="disabledBeforeTodayAndValue2EndDay"
+    >
+    </date-picker>
+    <date-picker
+      v-model="value2"
+      format="YYYY-MM-DD dddd"
+      type="date"
+      placeholder="結束時間"
+      :disabled-date="disabledBeforeToday"
+    >
+    </date-picker>
+    <date-picker
+      v-model="time1"
+      :minute-step="10"
+      format="a HH:mm"
+      type="time"
+      placeholder="HH:mm a"
+    ></date-picker>
+
+    <date-picker
+      v-model="time2"
+      :minute-step="10"
+      format="a HH:mm"
+      type="time"
+      placeholder="HH:mm a"
+    ></date-picker>
+    <button @click="printConsole()">按我一下</button>
   </div>
 </template>
 
 <script>
+const dayjs = require('dayjs');
+const birthday = new Date('March 13, 08 04:20');
+console.log(birthday.getHours());
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+import 'vue2-datepicker/locale/zh-cn';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: 'optionTest',
+  data: () => ({
+    value1: null,
+    value2: '2022-11-15',
+    time1: 'March 13, 08 04:20',
+    time2: 'March 13, 08 04:20',
+  }),
+  components: {
+    DatePicker,
+  },
+  methods: {
+    disabledBeforeTodayAndValue2EndDay(date) {
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      return date < today || date > this.value2;
+    },
+    disabledBeforeToday(date) {
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      return date < today || date <  (this.value1 ? this.value1 : "2100-11-15");
+    },
+
+    beforeEndTime(date) {
+      return date.getHours() < this.time2.getHours();
+    },
+    printConsole() {
+      console.log('value1:', this.value1, 'value2:', this.value2);
+      console.log(
+        'value1:',
+        dayjs(this.value1).format('YYYY-MM-DD'),
+        'value2:',
+        dayjs(this.value2).format('YYYY-MM-DD')
+      );
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style lang="scss" scoped></style>
